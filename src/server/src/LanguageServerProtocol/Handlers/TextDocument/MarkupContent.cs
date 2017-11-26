397575496487ba10c08795fb31d7af38f07cd4fe
SDmaN <sdman150296@gmail.com>
2017-11-26 14:07:22 +0400

    MarkupContent added.

---
+++ src/server/src/LanguageServerProtocol/Handlers/TextDocument/MarkupContent.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LanguageServerProtocol.Handlers.TextDocument
+{
+    public class MarkupContent
+    {
+        [JsonProperty("kind")]
+        public MarkupKind Kind { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ src/server/src/LanguageServerProtocol/Handlers/TextDocument/MarkupKind.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace LanguageServerProtocol.Handlers.TextDocument
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum MarkupKind
+    {
+        [EnumMember(Value = "plaintext")]
+        PlainText,
+
+        [EnumMember(Value = "markdown")]
+        Markdown
+    }
+}

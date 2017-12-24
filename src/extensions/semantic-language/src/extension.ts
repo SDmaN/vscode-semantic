ea5799c75093feb88e15906f948bb5cc0d8280d0
SDmaN <sdman150296@gmail.com>
2017-12-25 01:13:53 +0400

    launching

--- src/extensions/semantic-language/src/extension.ts
+++ src/extensions/semantic-language/src/extension.ts
@@ -2,46 +2,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as lsp from 'vscode-languageclient';
-import * as path from 'path';
-
-const isDebug = true;
+import { startLanguageServer } from './languageServer/lanuageServerStarter'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    let serverModule;
-
-    if(isDebug) {
-        serverModule = context.asAbsolutePath("../../server/src/PluginServer/bin/Debug/netcoreapp2.0/PluginServer.dll");
-    }
-
-    let serverWorkPath = path.dirname(serverModule);
-
-    let serverOptions: lsp.ServerOptions = {
-        run: { command: "dotnet", args: [ serverModule ], options: { cwd: serverWorkPath }},
-        debug: { command: "dotnet", args: [ serverModule ], options: { cwd: serverWorkPath }},
-    };
-
-    let clientOptions: lsp.LanguageClientOptions = {
-        documentSelector: [ {scheme: "file", language: "plaintext" } ],
-        synchronize: {
-            configurationSection: "semanticLanguage",
-            fileEvents: [
-                vscode.workspace.createFileSystemWatcher("**/.clientrc"),
-                vscode.workspace.createFileSystemWatcher("**/.demo")
-            ]
-        }
-    };
-
-    let client = new lsp.LanguageClient("semanticLanguageServer", "Semantic Language Server",
-        serverOptions, clientOptions);
-
-    // let disposable = client.start();
-
-    // context.subscriptions.push(disposable);
-
     console.log("Slang activated");
+
+    startLanguageServer(context);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/extensions/semantic-language/src/languageServer/lanuageServerStarter.ts
@@ -0,0 +1,38 @@
+import * as vscode from 'vscode';
+import * as lsp from 'vscode-languageclient';
+import * as path from 'path';
+
+const isDebug = true;
+
+export function startLanguageServer(context: vscode.ExtensionContext) {
+    let serverModule;
+
+    if(isDebug) {
+        serverModule = context.asAbsolutePath("../../language-server/src/PluginServer/bin/Debug/netcoreapp2.0/PluginServer.dll");
+    }
+
+    let serverWorkPath = path.dirname(serverModule);
+
+    let serverOptions: lsp.ServerOptions = {
+        run: { command: "dotnet", args: [ serverModule ], options: { cwd: serverWorkPath }},
+        debug: { command: "dotnet", args: [ serverModule ], options: { cwd: serverWorkPath }},
+    };
+
+    let clientOptions: lsp.LanguageClientOptions = {
+        documentSelector: [ {scheme: "file", language: "plaintext" } ],
+        synchronize: {
+            configurationSection: "semanticLanguage",
+            fileEvents: [
+                vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+                vscode.workspace.createFileSystemWatcher("**/.demo")
+            ]
+        }
+    };
+
+    let client = new lsp.LanguageClient("semanticLanguageServer", "Semantic Language Server",
+        serverOptions, clientOptions);
+
+    // let disposable = client.start();
+
+    // context.subscriptions.push(disposable);
+}

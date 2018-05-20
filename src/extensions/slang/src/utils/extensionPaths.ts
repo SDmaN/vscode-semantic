16549b82386d6de0703bac9d1f6f949fa108f145
SDmaN <sdman150296@gmail.com>
2018-05-20 20:24:02 +0400

    Uri in every path

--- src/extensions/slang/src/utils/extensionPaths.ts
+++ src/extensions/slang/src/utils/extensionPaths.ts
@@ -3,24 +3,24 @@ import { ExtensionContext, Uri } from "vscode";
 import { isDebug } from "../enviroment";
 
 export class ExtensionPaths {
-    public readonly base: string;
-    public readonly viewsMarkup: string;
-    public readonly templates: string;
+    public readonly base: Uri;
+    public readonly viewsMarkup: Uri;
+    public readonly templates: Uri;
 
     constructor(context: ExtensionContext) {
         const scriptsFolder = isDebug ? "src" : "out";
 
-        this.base = path.join(context.extensionPath);
-        this.viewsMarkup = path.join(this.base, scriptsFolder, "views", "markup");
-        this.templates = path.join(this.base, scriptsFolder, "templates");
+        this.base = Uri.file(path.join(context.extensionPath));
+        this.viewsMarkup = Uri.file(path.join(this.base.fsPath, scriptsFolder, "views", "markup"));
+        this.templates = Uri.file(path.join(this.base.fsPath, scriptsFolder, "templates"));
     }
 
     public getViewPath(relativePath: string) {
-        return Uri.file(path.join(this.viewsMarkup, relativePath));
+        return Uri.file(path.join(this.viewsMarkup.fsPath, relativePath));
     }
 
     public getTemplatePath(relativePath: string) {
-        return Uri.file(path.join(this.templates, relativePath));
+        return Uri.file(path.join(this.templates.fsPath, relativePath));
     }
 }
 
